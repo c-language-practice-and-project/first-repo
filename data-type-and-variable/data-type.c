1c6246de0157f56d4e7de20232b16ee873ff27a1
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-04-08 00:52:59 +0600

    variable name convention

---
+++ data-type-and-variable/data-type.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+    int alurPrice = 500;
+    int penPrice = 10;
+    int ChinirPrice = 30;
+    //printf("hellow");
+    //printf("%d", alurPrice);
+    //int egg = 4;
+    //printf("%d", egg);
+    int age;
+    printf("%d", age);
+    age = 23;
+    printf("%d", age);
+    return 0;
+}
