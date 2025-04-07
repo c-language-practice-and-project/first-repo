e79e3e15d9906970832850652438395672a66f2b
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-04-08 00:20:57 +0600

    first commit

---
+++ frist_print.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    printf("\t Hello World\n\n");
+    printf("Habibjx\n\n");
+    return 0;
+}
---
+++ second_file.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    printf("I'm a \"Programmer\"");
+    return 0;
+}
+
+//1. \n = new line
+//2. \t = tap
+
+// multiLine comment
+/*
+    1. this is multiLine comment.
+*/
