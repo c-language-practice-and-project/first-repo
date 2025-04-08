d1215bb1cddd2baeb6341d768864ece938948415
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-04-08 23:09:58 +0600

    float variable

---
+++ data-type-and-variable/float-integer.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    //int age = 23;
+    //printf("%d", age);
+    float sugerPrice = 10.80;
+    float penPrice = 5.20;
+    //printf("%f", penPrice);
+    //printf("%.1f", sugerPrice);
+    printf("%.2f", penPrice);
+    return 0;
+}
