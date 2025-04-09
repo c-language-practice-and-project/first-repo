535f05005128bf58646ed02d3190fb7bb1cc76c7
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-04-09 23:12:21 +0600

    review previous class

---
+++ data-type-and-variable/char-data.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main(){
+
+    //int a = 10, b = 12, c = 14;
+    //printf("%d", a, b);
+    //printf("%d%d", a, b);
+    //printf("%d\n%d", a, b);
+    //printf("%d%d%d", a,b,c);
+    //printf("%d\n%d\n%d", a, b , c);
+
+
+
+    //int z = 20;
+    //printf("%d", z);
+    //z = 30;
+    //printf("%d", z);
+    //z = 40;
+    //printf("%d", z);
+
+
+    //float p = 5.5;
+    //printf("%.2f", p);
+    //p = 10.22;
+    //printf("%f", p);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
