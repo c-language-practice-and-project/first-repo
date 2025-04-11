e293d8de11f9fce20bf58fdf40450eb2cf1ae89b
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-04-11 23:31:01 +0600

    scanf, gets, fgets

---
+++ data-type-and-variable/array-methods.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+int main(){
+    //char nameLatter = 'H';
+    //printf("%c", nameLatter);
+
+    //char name[] = {'H', 'a','b','i','b', 'f','\0'};
+    //printf("%s", name);
+
+    //char name[] = "Habib Fency";
+    //printf("%s", name);
+
+    //char name[] = "Habib Fency";
+    //printf("%c", name[4]);
+
+    //char arr[10];
+    //scanf("%s", &arr);
+    //printf("%s", arr);
+
+    //char name[5];
+    //scanf("%s", &name);
+    //printf("%s", name);
+
+    //char name[5];
+    //scanf("%4s", &name);
+    //printf("%s", name);
+
+    //char name[10];
+    //gets(name);
+    //printf("%s", name);
+
+    //char name[10];
+    //fgets(name, 5, stdin);
+    //printf("%s", name);
+
+    //char name[10];
+    //fgets(name, sizeof(name), stdin);
+    //printf("%s", name);
+
+    //char name[] = "habib fency\n";
+    //printf("%s", name);
+    //name[6] = 'G';
+    //printf("%s", name);
+
+
+
+
+
+    return 0;
+}
