de406081d21b1bc9390374b02581a74f90f99de4
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-04-11 12:20:20 +0600

    array

---
+++ data-type-and-variable/character-array-and-string.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+    //int age = 23;
+    //printf("%d", age);
+
+    //char name = 'H';
+    //printf("%c", name);
+
+    //char name[] = {'H', 'a', 'b', 'i', 'b'};
+    //printf("%s", name);
+
+    //char name[] = {'H', 'a', 'b', 'i', 'b', '\0'};
+    //printf("%s", name);
+
+    //char name[] = "I'm Habib from Bangladesh";
+    //printf("%s", name);
+
+    //char name[5] = "hello";
+    //printf("%s", name);
+
+    //char name[6] = "hello";
+    //printf("%s", name);
+
+
+
+
+
+
+    return 0;
+}
