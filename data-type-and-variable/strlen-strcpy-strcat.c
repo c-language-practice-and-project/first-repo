1e486d55ced16029c3d8aa6b27c1e604950aee3b
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-04-13 22:54:38 +0600

    how to find a string length width function and strlent fuction

---
+++ data-type-and-variable/strlen-strcpy-strcat.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+    //char name[] = {'H', 'a', 'b', 'i', 'b', '\0'};
+    //printf("%s", name);
+
+    /*
+    int i;
+    for(i = 0; i < 5; i++){
+        printf("%d\n", i);
+    }
+    */
+
+    /*
+    char arr[100];
+    scanf("%s", &arr);
+
+    int i;
+    for(i=0; arr[i] != '\0'; i++){
+
+    }
+     printf("%d", i); */
+
+
+    /*
+     char name[100];
+     scanf("%s", &name);
+
+     int length = strlen(name);
+     printf("%d", length);
+    */
+
+    /*
+    char arr[100];
+    scanf("%s", &arr);
+    int length = strlen(arr);
+    printf("%d", length);
+    */
+
+
+
+
+
+    return 0;
+}
