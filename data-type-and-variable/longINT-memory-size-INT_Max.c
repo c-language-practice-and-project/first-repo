5c25d8f867a6a4d01d27b3631931d2165aabc19a
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-04-14 23:10:22 +0600

    long long ing, memory size, INT max

---
+++ data-type-and-variable/longINT-memory-size-INT_Max.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<string.h>
+#include<limits.h>
+#include<float.h>
+int main(){
+    //printf("Max size of int = %d", INT_MAX);
+    //int x = 2147483647;
+    //int y = 2147483648;
+    //printf("%d %d", x, y);
+
+    //printf("Memory size of INT = %d", sizeof(int));
+
+    //long int x = 21474836478;
+    //int y = 2147483648;
+    //printf("%ld", x);
+
+    //long long int x = 21474836478;
+    //int y = 2147483648;
+    //printf("%lld", x);
+
+    //printf("long long int size = %zu", sizeof(long long int));
+    //printf("long long int max digit: %lld", LLONG_MAX);
+
+
+    //long long int x = 9223372036854775807;
+    //printf("%lld", x);
+
+    //long long int x = 9223372036854775809;
+    //printf("%lld", x);
+
+    //printf("Memory size of float: %zu", sizeof(float));
+    //printf("Max size of float : %e", FLT_MAX);
+
+
+
+
+
+    return 0;
+}
