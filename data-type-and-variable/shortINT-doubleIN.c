7671607c37e7b9ebe25cb43d848887f817ed5b5b
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-04-16 23:11:43 +0600

    shortINT & double float

---
+++ data-type-and-variable/shortINT-doubleIN.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<float.h>
+#include<limits.h>
+#include<string.h>
+
+
+int main(){
+    //short int x = 32767;
+    //printf("%hd", x);
+    //printf("Memory size of short int: %d", sizeof(short int));
+    //printf("Max value of short int: %zu ", SHRT_MAX);
+
+    //float x = 13.343;
+    //printf("%f", x);
+
+    //double x = 290938.3940;
+    //printf("%lf", x);
+
+
+    return 0;
+}
